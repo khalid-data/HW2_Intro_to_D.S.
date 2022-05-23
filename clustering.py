b2903784ad4f05f5fc2f8468fe48013cee2cc3f7
khali <khalid.tafla@campus.technion.ac.il>
2022-05-23 11:15:39 +0300

    second commit

---
+++ clustering.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+np.random.seed(2)
+
+
+def add_noise(data):
+    """
+    :param data: dataset as numpy array of shape (n, 2)
+    :return: data + noise, where noise~N(0,0.01^2)
+    """
+    noise = np.random.normal(loc=0, scale=0.01, size=data.shape)
+    return data + noise
+
+
+def choose_initial_centroids(data, k):
+    """
+    :param data: dataset as numpy array of shape (n, 2)
+    :param k: number of clusters
+    :return: numpy array of k random items from dataset
+    """
+    n = data.shape[0]
+    indices = np.random.choice(range(n), k, replace=False)
+    return data[indices]
+
+
+# ====================
+def transform_data(df, features):
+    """
+    Performs the following transformations on df:
+        - selecting relevant features
+        - scaling
+        - adding noise
+    :param df: dataframe as was read from the original csv.
+    :param features: list of 2 features from the dataframe
+    :return: transformed data as numpy array of shape (n, 2)
+    """
+
+    array1 = df[features[0]].to_numpy()
+    array2 = df[features[1]].to_numpy()
+    sum1 = np.sum(array1)
+    sum2 = np.sum(array2)
+    min1 = np.min(array1)
+    min2 = np.min(array2)
+    for i in range(array1.size):
+        array1[i] = (array1[i] - min1) / sum1
+
+    for j in range(array2.size):
+        array2[j] = (array2[j] - min2) / sum2
+
+    transformed_data = np.vstack((array1, array2))
+    transformed_data = add_noise(transformed_data)
+    print(transformed_data)
+    return transformed_data
+
+
+
+def kmeans(data, k):
+    """
+    Running kmeans clustering algorithm.
+    :param data: numpy array of shape (n, 2)
+    :param k: desired number of cluster
+    :return:
+    * labels - numpy array of size n, where each entry is the predicted label (cluster number)
+    * centroids - numpy array of shape (k, 2), centroid for each cluster.
+    """
+    pass
+    # return labels, centroids
+
+
+def visualize_results(data, labels, centroids, path):
+    """
+    Visualizing results of the kmeans model, and saving the figure.
+    :param data: data as numpy array of shape (n, 2)
+    :param labels: the final labels of kmeans, as numpy array of size n
+    :param centroids: the final centroids of kmeans, as numpy array of shape (k, 2)
+    :param path: path to save the figure to.
+    """
+    pass
+    # plt.savefig(path)
+
+
+def dist(x, y):
+    """
+    Euclidean distance between vectors x, y
+    :param x: numpy array of size n
+    :param y: numpy array of size n
+    :return: the euclidean distance
+    """
+    pass
+    # return distance
+
+
+def assign_to_clusters(data, centroids):
+    """
+    Assign each data point to a cluster based on current centroids
+    :param data: data as numpy array of shape (n, 2)
+    :param centroids: current centroids as numpy array of shape (k, 2)
+    :return: numpy array of size n
+    """
+    pass
+    # return labels
+
+
+def recompute_centroids(data, labels, k):
+    """
+    Recomputes new centroids based on the current assignment
+    :param data: data as numpy array of shape (n, 2)
+    :param labels: current assignments to clusters for each data point, as numpy array of size n
+    :param k: number of clusters
+    :return: numpy array of shape (k, 2)
+    """
+    pass
+    # return centroids
+
